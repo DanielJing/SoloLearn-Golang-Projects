20e09ebb4b1a92c510915db8d366826c470b9413
Daniel Jing <jpdan727@gmail.com>
2023-06-05 16:46:06 +0800

    Update match results.go

--- match results.go
+++ match results.go
@@ -1,39 +1,21 @@
 package main
-
 import "fmt"
-
 func main() {
-
 	results := []string{"w", "l", "w", "d", "w", "l", "l", "l", "d", "d", "w", "l", "w", "d"}
-
 	var a int
-
 	lastMatchResult := ""
-
 	fmt.Scanln(&lastMatchResult)
-
 	results = append(results, lastMatchResult)
 
 	for _, v := range results {
-
 		switch {
-
 		case v == "w":
-
 			a += 3
-
 		case v == "l":
-
 			a += 0
-
 		case v == "d":
-
 			a += 1
-
 		}
-
 	}
-
 	fmt.Println(a)
-
 }
